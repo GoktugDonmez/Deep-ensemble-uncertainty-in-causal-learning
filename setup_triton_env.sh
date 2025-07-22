cb76e1d9c7cb743f72600bc3a4cd3cf2e56408ba
GoktugDonmez <goktudonmez@gmail.com>
2025-07-22 13:30:28 +0000

    add triton config

--- setup_triton_env.sh
+++ setup_triton_env.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
-# Setup script for Triton environment
+# Setup script for Triton environment following Triton best practices
 
 echo "Setting up environment for DiBS Deep Ensembles on Triton..."
 
-# Load mamba module (faster than conda)
+# Load mamba module (faster than conda, recommended by Triton)
 module load mamba
+
+# Configure conda to use work directory instead of home (for quota reasons)
+echo "Configuring conda directories..."
+mkdir -p $WRKDIR/.conda_pkgs
+mkdir -p $WRKDIR/.conda_envs
+
+conda config --append pkgs_dirs ~/.conda/pkgs
+conda config --append envs_dirs ~/.conda/envs
+conda config --prepend pkgs_dirs $WRKDIR/.conda_pkgs
+conda config --prepend envs_dirs $WRKDIR/.conda_envs
 
 # Create a new environment specifically for this project
 # Using the DiBS requirements but with a project-specific name
+echo "Creating causal_experiments environment..."
 mamba env create -f dibs/environment.yml -n causal_experiments
 
 # Activate the environment
 source activate causal_experiments
 
 # Install additional dependencies needed for your project
+echo "Installing additional dependencies..."
 pip install pyyaml  # For YAML config files
 
 # Install DiBS in development mode from the submodule
+echo "Installing DiBS in development mode..."
 cd dibs
 pip install -e .
 cd ..
@@ -24,5 +37,5 @@ cd ..
 echo "Environment setup complete!"
 echo "To use this environment on Triton:"
 echo "1. module load mamba"
-echo "2. conda activate causal_experiments"
+echo "2. source activate causal_experiments"
 echo "3. export PYTHONPATH=\"\${PYTHONPATH}:\${PWD}/dibs\"" 
